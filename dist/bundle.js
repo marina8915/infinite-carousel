a372e915fdccbb4e288516ecdaf9bba368cfb5f7
marina <marina_bakum@mail.ru>
2018-02-21 05:37:29 +0200

    check img active

--- dist/bundle.js
+++ dist/bundle.js
@@ -85,28 +85,33 @@ function component() {
     //array slider
     Array.prototype.forEach.call(document.getElementsByClassName('slider'), function (event) {
         event.addEventListener('mousedown', function (event) {
-            //determination of slider id, slide
+            //determination of slider id, activeSlider, slide
             var id = event.path[1].getAttribute('id');
+            var activeSlider = document.getElementById(id);
             var slide = event.target || event.srcElement;
-            //beginning of move
-            document.onmousemove = function (e) {
-                slide.style.left = e.clientX + 'px';
-                console.log(e.clientX);
-            };
-            //end of move
-            document.onmouseup = document.onmouseout = function (e) {
-                //check limit
-                var limits = {
-                    right: 200,
-                    left: 0
+            //assign the last element of slider class active
+            var countImg = activeSlider.getElementsByTagName('img').length;
+            activeSlider.getElementsByTagName('img')[countImg - 1].classList.add('active');
+            //check if image active then we can move it
+            if (slide.classList.contains('active')) {
+                //beginning of move
+                document.onmousemove = function (e) {
+                    slide.style.left = e.clientX + 'px';
                 };
-                if (e.clientX > limits.right || e.clientX < limits.left) {
-                    document.getElementById(id).insertBefore(slide, document.getElementById(id).childNodes[0]);
-                }
-                slide.style.left = 0 + 'px';
-                document.onmousemove = null;
-                document.onmouseup = null;
-            };
+                //end of move
+                document.onmouseup = document.onmouseout = function () {
+                    //check if bias more then limit - change position images
+                    var bias = slide.style.left.split('px');
+                    var limit = 200;
+                    if (bias[0] > limit) {
+                        slide.style.left = 0 + 'px';
+                        slide.classList.remove('active');
+                        activeSlider.insertBefore(slide, activeSlider.childNodes[0]);
+                    }
+                    document.onmousemove = null;
+                    document.onmouseup = null;
+                };
+            }
         });
     });
     return element;
